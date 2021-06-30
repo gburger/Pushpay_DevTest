48f867d352aa0b25af5549a7dd7cca2429d8b568
gburger <47358982+gburger@users.noreply.github.com>
2021-06-30 16:38:08 -0400

    Add files via upload

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+// TODO: define interface
+export interface PersonType {
+    name: string
+    height: string
+    mass: string
+    birth_year: string
+    homeworld: string
+    films: string[]
+    species: string
+}
