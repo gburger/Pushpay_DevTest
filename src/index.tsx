f672ae6221434a24d18545227119a9031b0557a0
gburger <47358982+gburger@users.noreply.github.com>
2021-07-02 01:52:17 -0600

    Add files via upload

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import 'bootstrap/dist/css/bootstrap.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
 
-import './index.css'
-import App from './components/App'
+import "./index.css";
+import App from "./components/App";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-)
+  <React.StrictMode>
+    <div
+      style={{
+        backgroundImage: `url("https://starwarsblog.starwars.com/wp-content/uploads/2020/04/star-wars-backgrounds-25.jpg")`,
+        margin: 0,
+      }}
+    >
+      <App />
+    </div>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
