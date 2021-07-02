7180f1103ae8a25b0c17cad2cc4abd7051a2af43
gburger <47358982+gburger@users.noreply.github.com>
2021-07-02 01:51:05 -0600

    Add files via upload

--- src/components/Person/Person.test.tsx
+++ src/components/Person/Person.test.tsx
@@ -1,14 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from "@testing-library/react";
 
-import { PersonType } from '../../types';
-import Person from './Person'
+import { PersonType } from "../../types";
+import Person from "./Person";
 
-describe('<Person />', () => {
-  test('should render the person\'s name', () => {
-    const person: PersonType = { name: 'Jek Tono Porkins'}
+describe("<Person />", () => {
+  const person: PersonType = {
+    name: "Jek Tono Porkins",
+    height: "180",
+    mass: "110",
+    birth_year: "896BBY",
+    homeworld: "Bestine IV",
+    films: ["A New Hope"],
+    species: "Human",
+  };
+  test("should render a person object", () => {
+    render(<Person person={person} />);
 
-    render(<Person person={person} />)
-
-    screen.getByText(person.name)
-  })
+    expect(screen.getByText(person.name)).toHaveTextContent("Jek Tono Porkins");
+  });
 });
--- src/components/Person/Person.tsx
+++ src/components/Person/Person.tsx
@@ -1,54 +1,60 @@
-import React, { useState } from 'react'
-import { PersonType } from '../../types'
+import React, { useState } from "react";
+import { PersonType } from "../../types";
 
-import Accordion from 'react-bootstrap/Accordion'
-import Card from 'react-bootstrap/Card'
-import Figure from 'react-bootstrap/Figure'
-import Image from 'react-bootstrap/Image'
-import Col from 'react-bootstrap/Col'
-import Row from 'react-bootstrap/Row'
+import { Accordion } from "react-bootstrap";
+import { Card } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import { Row } from "react-bootstrap";
 
 interface PersonProps {
-    person: PersonType
+  person: PersonType;
 }
 
 function Person({ person }: PersonProps) {
-    return (
-        <div>
-            <Card>
-                <Accordion.Toggle as={Card.Header} eventKey={person.name}>
-                    {person.name}
-                </Accordion.Toggle>
-                <Accordion.Collapse eventKey={person.name}>
-                    <Card.Body>
-                        <Row>
-                            <Col>
-                                Height: {person.height} cm <br />
-                                Weight: {person.mass} kg <br />
-                                Species: {person.species} <br />
-                                Birth Year: {person.birth_year} <br />
-                                Homeworld: {person.homeworld} <br />
-                            </Col>
-                            <Col>
-                                <Image
-                                    src={
-                                        process.env.PUBLIC_URL +
-                                        '/' +
-                                        person.name +
-                                        '.png'
-                                    }
-                                    className="center"
-                                />
-                            </Col>
-                        </Row>
-                        <Row>
-                            Apearned in: {person.films} <br />
-                        </Row>
-                    </Card.Body>
-                </Accordion.Collapse>
-            </Card>
-        </div>
-    )
+  return (
+    <div>
+      <Card>
+        <Accordion.Toggle as={Card.Header} eventKey={person.name}>
+          {person.name}
+        </Accordion.Toggle>
+        <Accordion.Collapse eventKey={person.name}>
+          <Card.Body>
+            <Row>
+              <Col>
+                <Row>
+                  Height: {person.height} cm <br />
+                </Row>
+
+                <Row>
+                  Weight: {person.mass} kg <br />
+                </Row>
+                <Row>
+                  Species: {person.species} <br />
+                </Row>
+                <Row>
+                  Birth Year: {person.birth_year} <br />
+                </Row>
+                <Row>
+                  Homeworld: {person.homeworld} <br />
+                </Row>
+                <Row>
+                  Apearned in:{" "}
+                  <ul className="m-2">
+                    {person.films.map((films) => (
+                      <li>{films} </li>
+                    ))}
+                  </ul>
+                </Row>
+              </Col>
+              <Col>
+                <img src={`../img/${person.name.replace(/\s/g, "")}.png`}></img>
+              </Col>
+            </Row>
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    </div>
+  );
 }
 
-export default Person
+export default Person;
