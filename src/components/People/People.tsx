321117cdaa41b16a140b23fc277b736c3ee87414
gburger <47358982+gburger@users.noreply.github.com>
2021-07-02 01:50:42 -0600

    Add files via upload

--- src/components/People/People.tsx
+++ src/components/People/People.tsx
@@ -1,125 +1,97 @@
-import React, { Component } from 'react'
-import { Accordion } from 'react-bootstrap'
+import React from "react";
+import { Accordion } from "react-bootstrap";
 
-import { fetchJson } from '../../api'
-import { PersonType } from '../../types'
-import Person from '../Person'
-
-async function getPerson(id: number) {
-    let person: PersonType
-
-    person = {
-        name: '',
-        height: '',
-        mass: '',
-        birth_year: '',
-        homeworld: '',
-        species: '',
-        films: [''],
-    }
-    ;(async () => {
-        await fetchJson<{
-            name: string
-            height: string
-            mass: string
-            birth_year: string
-            homeworld: string
-            films: string[]
-            species: string
-        }>('people/' + id)
-            .then((Response) => {
-                person.name = Response.name
-                person.height = Response.height
-                person.mass = Response.mass
-                person.birth_year = Response.birth_year
-                person.homeworld = Response.homeworld
-                person.species = Response.species
-                person.films = Response.films
-            })
-            .catch(() => {
-                console.log('Person id' + id + 'missing')
-                return
-            })
-            .then(() => {
-                if (person.species.length === 0) {
-                    person.species = 'Human'
-                } else {
-                    let url = person.species[0].substring(
-                        'https://swapi.dev/api/'.length
-                    )
-                    fetchJson<{ name: string }>(url).then((Response) => {
-                        person.species = Response.name
-                    })
-                }
-            })
-            .then(() => {
-                let url = person.homeworld.substring(
-                    'https://swapi.dev/api/'.length
-                )
-                fetchJson<{ name: string }>(url).then((Response) => {
-                    person.homeworld = Response.name
-                })
-            })
-            .then(() => {
-                for (let i = 1; i < person.films.length; i++) {
-                    let url = person.films[i].substring(
-                        'https://swapi.dev/api/'.length
-                    )
-                    fetchJson<{ title: string }>(url).then((Response) => {
-                        person.films[i] = Response.title
-                    })
-                }
-            })
-    })()
-    return person
-}
+import { fetchJson } from "../../api";
+import { getPerson } from "../../api";
+import { PersonType } from "../../types";
+import Person from "../Person";
 
 function People() {
-    const [people, setPeople] = React.useState<PersonType[]>([])
-    console.log('i have started')
-    React.useEffect(() => {
-        let numPeople = 0
-        let temp: PersonType[] = [
-            {
-                name: '',
-                height: '',
-                mass: '',
-                birth_year: '',
-                homeworld: '',
-                films: [''],
-                species: '',
-            },
-        ]
+  const [people, setPeople] = React.useState<PersonType[]>([]);
+  const [search, setSearch] = React.useState("");
 
-        ;(async () => {
-            await fetchJson<{ count: string }>('/people').then((Response) => {
-                numPeople = parseInt(Response.count)
-            })
-        })()
-        ;(async () => {
-            console.log(numPeople)
-            for (let i = 1; i < 82; i++) {
-                if (i === 17) continue
-                await getPerson(i).then((person) => {
-                    //let temp: PersonType[] = people
+  React.useEffect(() => {
+    let numPeople = 0;
 
-                    temp[i - 1] = person
-                    setPeople(temp)
-                })
+    (async () => {
+      await fetchJson<{ count: string }>("/people")
+        .then((Response) => {
+          numPeople = parseInt(Response.count, 10);
+        })
+        .then(() => {
+          (async () => {
+            //create tempory array to buil people list app place holder at 0
+            let temp: PersonType[] = [
+              {
+                name: "",
+                height: "",
+                mass: "",
+                birth_year: "",
+                homeworld: "",
+                films: [""],
+                species: "",
+              },
+            ];
+            //needs optimization very slow
+            for (let i = 1; i < numPeople; i++) {
+              await getPerson(i).then((person) => {
+                temp[i] = person;
+              });
             }
-        })()
-    }, [])
+            //remove place holder first element
+            temp.splice(0, 1);
+            //verify data, remove anything broken
+            for (let i = 0; i < temp.length; i++) {
+              if (
+                typeof temp[i].name == "undefined" ||
+                typeof temp[i].mass == "undefined" ||
+                typeof temp[i].height == "undefined" ||
+                typeof temp[i].species == "undefined" ||
+                typeof temp[i].homeworld == "undefined" ||
+                typeof temp[i].films == "undefined"
+              ) {
+                temp.splice(i, 1);
+              }
+            }
+            setPeople(temp);
+          })();
+        });
+    })();
+  }, []);
 
-    return (
-        <div className="d-flex justify-content-center">
-            <Accordion>
-                {console.log(people)}
-                {people.map((person) => (
-                    <Person person={person} />
-                ))}
-            </Accordion>
-        </div>
-    )
+  return (
+    <div className="m-0">
+      <div className="d-flex justify-content-center m-2">
+        <input
+          type="text"
+          placeholder="Search for a person..."
+          onChange={(event) => {
+            setSearch(event.target.value);
+          }}
+        />
+      </div>
+
+      <div className="d-flex justify-content-center m-0">
+        <Accordion>
+          {people
+            .filter((peopleList) => {
+              //if search bar empty return full list
+              if (search == "") {
+                return peopleList;
+                //return filter list
+              } else if (
+                peopleList.name.toLowerCase().includes(search.toLowerCase())
+              ) {
+                return peopleList;
+              }
+            })
+            .map((peopleList) => (
+              <Person key="{index.Of(peopleList)}" person={peopleList} />
+            ))}
+        </Accordion>
+      </div>
+    </div>
+  );
 }
 
-export default People
+export default People;
