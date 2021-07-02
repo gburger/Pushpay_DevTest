0dddc6c68d19c177e7cee8d1afa89fcc83072f79
gburger <47358982+gburger@users.noreply.github.com>
2021-07-02 01:48:52 -0600

    Add files via upload

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,18 @@
-import People from '../People';
+import People from "../People";
 
 function App() {
   return (
-    <People />
+    <div>
+      <header className="m-0" />
+      <div
+        style={{
+          backgroundImage: `url("https://starwarsblog.starwars.com/wp-content/uploads/2020/04/star-wars-backgrounds-25.jpg")`,
+          margin: 0,
+        }}
+      >
+        <People />;
+      </div>
+    </div>
   );
 }
 
