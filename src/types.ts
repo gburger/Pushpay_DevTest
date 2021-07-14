2e6502cbc3e39047262f15cb8ae44b14bc48225f
gburger <47358982+gburger@users.noreply.github.com>
2021-07-13 22:07:04 -0400

    Add files via upload

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,15 @@
 export interface PersonType {
-    name: string
-    height: string
-    mass: string
-    birth_year: string
-    homeworld: string
-    films: string[]
-    species: string
+  name: string;
+  height: string;
+  mass: string;
+  birth_year: string;
+  homeworld: string;
+  films: string[];
+  species: string;
+  addInfo: boolean;
+}
+
+export interface PeoplePage {
+  results: PersonType[];
+  next: string;
 }
